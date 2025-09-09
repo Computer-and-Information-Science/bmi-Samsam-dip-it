d91fb8587f3eba176ef38922503da8b2ab602ecb
KyawBhoneNaing <kyawbnaing@gmail.com>
2025-09-09 20:50:24 +0000

    code to compute GPI

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
- 
+    // declare my variables
+    int height;
+    height  = 61; 
 
+    int weight= 130;
 
+    double weightKg = weight*0.453592;
+    double heightMeters = height*0.0254;
+
+    double bmi = weightKg / (heightMeters * heightMeters);
+
+    cout << "The BMI for this person is : " << bmi << "." << endl;
 }
+
+
